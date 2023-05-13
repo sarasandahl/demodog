746c41c02647a41bc15f86236e33bcccb2055a1d
sarasandahl <92533105+sarasandahl@users.noreply.github.com>
2023-05-13 21:38:06 +0200

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # demo-dog-php
 A demo of OOP in PHP for beginners with PHP and OOP (and a smidge of MVC).
+
+1. Lägg till breed
+Alla hundar ska få en ras (eng breed). Uppdatera Dog-klassen så den lagrar breed per hund. Uppdatera konstruktorn samt getInfo. Uppdatera $myDogs = new DogHotell… så varje hund får en ras. Ngt mer som behöver uppdateras? Testkör och se att utskriften till HTML får med varje hunds ras.
+
+2. Lägg till vikt
+Alla hundar ska få en vikt. Uppdatera så att vi både lagrar och visar denna nya info. Vikt behöver vara ett decimaltal. Detta är vikten i kg.
+
+3. Lägg till höjd
+Alla hundar ska få en höjd. Uppdatera så att vi både lagrar och visar denna nya info. Höjd behöver vara ett decimaltal. Detta är mankhöjden i cm.
+
+4. Lägg till fler hundar
+Utöka din array av hundar med tre till. Visas alla när du öppnar sidan i webbläsaren? Visas all info om alla hunder, inklusive de nya breed, vikt och höjd?
+
+5. Skriv ut alla hundnamn
+Längst ner i body-taggen lägger vi till följande:
+
+    <h1>&#128054; My Dogs by Name &#128054;</h1>
+    <ul>
+    <?php
+        foreach($myDogs->getDogs() as $dog) {
+            echo $render->renderDogName($dog);
+        }
+    ?>
+    </ul>
+
+Notera att vi här anropar en ny metod renderDogName och tanken är att denna metoden ska skriva ut en li-tagg med hundens namn. För att lyckas med detta behövs ytterligare en ny metod getName i Dog-klassen.
+
+Skriv dessa nya metoder så att vi får en utskrift med bara hundarnas namn listade.
--- index.php
+++ index.php
@@ -10,29 +10,39 @@
 
 // DEL 1: Vår data och funktionalitet
 
-class Dog {
+class Dog { //klass Dog som håller/tilldelar värdena enskilda objekt(hund) ska ha. Objekten som tilldelas kommer ärva dessa värden men ha olika egenskaper
     // egenskap = en variabel inne i ett objekt
-    private $name = "";
-    private $color = "";
-    private $age = 0;
+    private $name = ""; //private, icke global pga all kod ska inte ha tillgång tilla kod. "" är en sträng. definerar vaiabel
+    private $color = ""; // "" är en sträng. definerar variabel
+    private $age = 0; // 0 är en int. definerar variabel
+    private $breed = "";
+    private $weight = 0;
+    private $height = 0;
 
-    // konstruktor
-    function __construct(string $name, string $color, int $age) {
-        $this->name = $name; // i JS this.name = name
+    // konstruktor. Konstruktor som definerar/skapar default värden för varje nytt objekt. Så vi senare i Doghotell vet att första värdet är name och string
+    function __construct(string $name, string $color, int $age, string $breed, float $weight, float $height) { // tar emot argumenten $name, $color, $age och med strin/int förytydligar deras värde
+        $this->name = $name; // i JS this.name = name. //säger att this.name är $name
         $this->color = $color;
         $this->age = $age;
+        $this->breed = $breed;
+        $this->weight = $weight;
+        $this->height = $height;
     }
 
-    // metod = funktion inne i objekt
-    function getInfo(): string {
-        return "Name: {$this->name}, Color: {$this->color}, Age: {$this->age}";
+    // metod = funktion inne i objekt som innehåller värdena vi skapat
+    function getInfo(): string { //objektet ska vara en string
+        return "Name: {$this->name}, Color: {$this->color}, Age: {$this->age}, Breed: {$this->breed}, Weight: {$this->weight}, Height: {$this->height}"; //returnera variabler och tilldelad konstruktor ihop. Definerar 
+    }
+
+    function getName(): string {
+        return "Name: {$this->name}";
     }
 }
 
-class DogHotell {
-    private $dogs = [];
+class DogHotell { //ny klass med vilka hundar som är inskrivna på hotellet
+    private $dogs = []; //deklarerar arrayen av hundar. Tom, ska passa in värden
 
-    function __construct($dogs) {
+    function __construct($dogs) { //konstruktor som säger att variabel $dogs är dogs genom att passa in den som ett argument. Constuctor definerar de framtida objekten
         $this->dogs = $dogs;
     }
 
@@ -41,25 +51,38 @@ class DogHotell {
     }
 }
 
-class RenderDog {
+class RenderDog { //klass för att rendera dog-objekten och skriva ut
 
-    function renderDogInfo(Dog $dog): string {
-        return "<p>{$dog->getInfo()}</p>";
+    function renderDogInfo(Dog $dog): string {// in i Dog-klassen  och säg att en $Dog med tilldelade värden är dog
+        return "<p>{$dog->getInfo()}</p>"; // kör funktionen getInfo i $dog för att få ut en och en
     }
 
 }
 
+class RenderName {
+
+    function renderDogName(Dog $dog): string {
+        return "<li>{$dog->getName()}</li>";
+    }
+}
+
+
 // DEL 2: Vår app
 
 //$testDog = new Dog("Pluto","orange", 12);
-$myDogs = new DogHotell([
-    new Dog("Pluto","orange", 12),
-    new Dog("Pricken","svart-vit", 3),
-    new Dog("Lady","beige", 5),
-    new Dog("Fox","red", 4)
+$myDogs = new DogHotell([ //array med hundar som tilldelas i klassen DogHotell
+    new Dog("Pluto","orange", 12, "Airdale terrier", 13.416, 42.340), //enskild ny hund in och körs med konstruktorn för att defineras i klassen Dog
+    new Dog("Pricken","svart-vit", 3, "Amstaff", 16.783, 35.043),
+    new Dog("Lady","beige", 5, "Tax", 6.902, 16.04),
+    new Dog("Fox","red", 4, "Varghund", 23.905, 57.943),
+    new Dog("Marre","svart", 1, "Amstaff", 9.695, 27.034),
+    new Dog("Voff","rosa", 10, "Dalmatin", 14.506, 35.104),
+    new Dog("Hund","brown", 5, "Labrador", 13.943, 38.679),
 ]);
 
 $render = new RenderDog();
+$renders = new RenderName();
+
 
 // DEL 3: Vår output = echo HTML
 ?>
@@ -79,10 +102,20 @@ $render = new RenderDog();
         //echo "<p>Name: {$testDog->name} </p>";
         //echo "<p>Color: {$testDog->color} </p>";
 
-        foreach($myDogs->getDogs() as $dog) {
-            echo $render->renderDogInfo($dog);
+        foreach($myDogs->getDogs() as $dog) { //loopa igenom myDogs arrayen som är new DogHotell med renderar lista
+            echo $render->renderDogInfo($dog); //skriv ut varje hund genom att gå in i funktionen render som i sin tur är klassen renderdog ...
+            //som i sin tur kör funktionen renderDogInfo som går in och skriver ut från/i klassen dog och definerart värden och egenskaper
         }
     ?>
+
+        <h1>&#128054; My Dogs by Name &#128054;</h1>
+        <ul>
+    <?php
+        foreach($myDogs->getDogs() as $dog) {
+                echo $renders->renderDogName($dog);
+        }
+    ?>
+        </ul>
 
 </body>
 </html>
